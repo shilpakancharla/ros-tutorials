38b9d939e33e2f571cd9b048acfa1c813146f288
Shilpa Kancharla <shilpakancharla18@gmail.com>
2020-09-30 12:20:54 -0400

    Create ROS Workspace

---
+++ ROS-CH5/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/shilpa/Documents/ros-tutorials/ROS-CH5/catkin_ws/src
+# Build directory: /home/shilpa/Documents/ros-tutorials/ROS-CH5/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ ROS-CH5/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/shilpa/Documents/ros-tutorials/ROS-CH5/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
