5e14e8af87aa1dc0e07617bcc0d3a724f06f1952
Shilpa Kancharla <shilpakancharla18@gmail.com>
2021-05-23 19:34:15 -0400

    Create atomic_object_demo.cpp

---
+++ cpp_parallel_concurrent_course/atomic_object_demo.cpp
@@ -0,0 +1,21 @@
+/**
+ * Two shoppers adding items to a shared notepad
+ */
+# include <thread>
+# include <atomic>
+
+std::atomic<unsigned int> garlic_count(0);
+
+void shopper() {
+  for (int i = 0; i < 1000000; i++) {
+    garlic_count++;
+  }
+}
+
+int main() {
+  std::thread nanami(shopper);
+  std::thread gojo(shopper);
+  nanami.join();
+  gojo.join();
+  printf("We should buy %u garlic.\n", garlic_count.load());
+}
